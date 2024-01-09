4c26e802cf3e6860e787f63f0c32a24a56efbfad
HIhammad <67950980+HIhammad@users.noreply.github.com>
2024-01-09 23:36:56 +0100

    reading help from help html file

--- finalAptProject/helpwindow.cpp
+++ finalAptProject/helpwindow.cpp
@@ -2,6 +2,9 @@
 #include <QTextStream>
 #include "helpwindow.h"
 
+#include "qloggingcategory.h"
+QLoggingCategory helpWindowCat("helpWindow");
+
 HelpWindow::HelpWindow(QWidget *parent) : QDialog(parent), label(new QLabel(this)) {
     QVBoxLayout *layout = new QVBoxLayout(this);
     label->setText("This is the help text...");
@@ -14,14 +17,24 @@ void HelpWindow::setHelpText(const QString &text) {
 }
 
 void HelpWindow::loadHelpContent() {
-    QFile helpFile(":/help.html"); // Path to your HTML file in the resources
-    if (helpFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        QTextStream stream(&helpFile);
-        label->setText(stream.readAll()); // Set HTML content on QLabel
-        helpFile.close();
-    } else {
-        label->setText("Help content could not be loaded.");
-    }
+    qCDebug(helpWindowCat) << "loadHelpContent() is called";
+       QString filePath = "help"; // If it's a file in the application's directory
+       QFile helpFile(filePath);
+       if (helpFile.exists()) {
+           qCDebug(helpWindowCat) << "File exists: " << filePath;
+           if (helpFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+               qCDebug(helpWindowCat) << "File open";
+               QTextStream stream(&helpFile);
+               label->setText(stream.readAll()); // Set HTML content on QLabel
+               helpFile.close();
+           } else {
+               qCDebug(helpWindowCat) << "Failed to open file";
+               label->setText("Help content could not be loaded.");
+           }
+       } else {
+           qCDebug(helpWindowCat) << "File does not exist: " << filePath;
+           label->setText("Help file not found.");
+       }
 }
 
 
